df104ba6bcdc7d37ea376268c4e535dd85762a42
skwhit <whitlowspencer@gmail.com>
2023-02-05 20:25:31 -0800

    added resume embed to landing section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-list");
+const openResume = document.querySelector(".open-resume-button");
+const closeResume = document.querySelector(".close-resume-button");
+const resume = document.querySelector(".resume-container");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -12,3 +15,20 @@ document.querySelectorAll(".nav-item").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
+openResume.addEventListener("click", () => {
+  resume.classList.add("resume-visible");
+  document.body.classList.add("overflow-hidden");
+});
+
+closeResume.addEventListener("click", () => {
+  resume.classList.remove("resume-visible");
+  document.body.classList.remove("overflow-hidden");
+  openResume.blur();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeResume.click();
+  }
+});
